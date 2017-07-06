94230cc73d0ac2bc97be03a32ea2645b8247d1b1
congzhaoyang <congzhaoyang1@163.com>
2017-07-06 14:25:38 +0800

    webpack demo, add css-loader,sass-loader,img-loader,babel-loader

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import Person from './Person.js';
+
+let p = new Person('congzhaoyang', 18);
+document.write(p.say());
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+   },
+   module: {
+     rules: [
+       {
+         test: /\.css$/,
+         use: [
+           'style-loader',
+           'css-loader'
+         ]
+       },
+       {
+         test: /\.(png|svg|jpg|gif)$/,
+         use: [
+           'file-loader'
+         ]
+       },
+       {
+         test: /\.(scss)$/,
+         use: [
+	   'style-loader',
+	   'css-loader',
+	   'sass-loader'
+         ]
+       },
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }
+    },
+    ],
+    }
+};
+
+
+
